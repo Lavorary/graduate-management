a21cd70761015117ae42d0126626bbd6f50f1cfc
Lavorary <hei.lavorary@gmail.com>
2026-08-14 11:01:18 +0300

    feat : add Exam model

---
+++ src/main/java/hei/school/app/model/Exam.java
@@ -0,0 +1,14 @@
+package hei.school.app.model;
+
+import lombok.Builder;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+@Builder
+public record Exam(
+        UUID id,
+        Instant dateExam,
+        BigDecimal coefficient
+) {}

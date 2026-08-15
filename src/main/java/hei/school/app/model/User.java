ace56098e61579da829838bf08850e3de3d2e3cc
Lavorary <hei.lavorary@gmail.com>
2026-08-15 23:44:29 +0300

    feat : add User model

---
+++ src/main/java/hei/school/app/model/User.java
@@ -0,0 +1,13 @@
+package hei.school.app.model;
+
+import hei.school.app.enums.Roles;
+
+import java.util.UUID;
+
+public record User(
+        UUID id,
+        String firstName,
+        String lastName,
+        Roles role,
+        String email,
+        String password) {}

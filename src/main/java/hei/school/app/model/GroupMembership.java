283a802a07ec78e9f6f4cf871a6e845e634998b5
Diarilala <hei.diarilala@gmail.com>
2026-08-16 22:29:18 +0300

    feat: add domain GroupMembership

---
+++ src/main/java/hei/school/app/model/GroupMembership.java
@@ -0,0 +1,9 @@
+package hei.school.app.model;
+
+import java.time.LocalDate;
+import java.util.UUID;
+import lombok.Builder;
+
+@Builder
+public record GroupMembership(
+    UUID id, LocalDate startDate, LocalDate endDate, User user, Group group) {}

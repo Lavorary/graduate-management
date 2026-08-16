ecaa598dc8ff79392522eee89af0f45cf1807c29
Diarilala <hei.diarilala@gmail.com>
2026-08-17 01:10:59 +0300

    feat: add Role enum

---
+++ src/main/java/hei/school/app/security/model/UserRole.java
@@ -0,0 +1,23 @@
+package hei.school.app.security.model;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public enum UserRole implements GrantedAuthority {
+    ADMIN,
+    TEACHER,
+    STUDENT;
+
+    public String role() {
+        return name();
+    }
+
+    @Override
+    public String getAuthority() {
+        return "ROLE_" + name();
+    }
+
+    @Override
+    public String toString() {
+        return role();
+    }
+}

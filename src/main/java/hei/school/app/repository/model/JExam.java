fd5f5b5bb230be84e53649fca3ea1209ce6e6772
Diarilala <hei.diarilala@gmail.com>
2026-08-17 01:26:49 +0300

    feat: add exam entity

---
+++ src/main/java/hei/school/app/repository/model/JExam.java
@@ -0,0 +1,43 @@
+package hei.school.app.repository.model;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "exam")
+@Builder
+public class JExam {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(name = "exam_date", nullable = false)
+  private Instant examDate;
+
+  @Column(name = "coefficient", nullable = false, precision = 10, scale = 2)
+  private BigDecimal coefficient;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "course_id")
+  private JCourse course;
+}

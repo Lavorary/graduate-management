e0e621abed6c22ccf28ac2beefb02a4ecbaf8808
Diarilala <hei.diarilala@gmail.com>
2026-08-17 01:28:25 +0300

    feat: add group entity

---
+++ src/main/java/hei/school/app/repository/model/JGroup.java
@@ -0,0 +1,42 @@
+package hei.school.app.repository.model;
+
+import java.util.Set;
+import java.util.UUID;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "group")
+@Builder
+public class JGroup {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(name = "ref", nullable = false)
+  private String ref;
+
+  @ManyToMany
+  @JoinTable(
+      name = "group_cursus",
+      joinColumns = @JoinColumn(name = "group_id"),
+      inverseJoinColumns = @JoinColumn(name = "cursus_id"))
+  private Set<JCursus> cursus;
+}

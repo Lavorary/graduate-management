163383bc29401989e5d0786af01e0c61d8f54d4a
Diarilala <hei.diarilala@gmail.com>
2026-08-16 22:32:46 +0300

    feat: add entity JCursus

---
+++ src/main/java/hei/school/app/repository/model/JCursus.java
@@ -0,0 +1,36 @@
+package hei.school.app.repository.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "cursus")
+@Builder
+public class JCursus {
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @Column(name = "name", nullable = false)
+  private String name;
+
+  @Column(name = "description", columnDefinition = "TEXT", nullable = false)
+  private String description;
+
+  @Column(name = "year", nullable = false)
+  private String year;
+}

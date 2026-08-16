52234f73fa1491aa818e88f0b468cbea8b41ef83
Diarilala <hei.diarilala@gmail.com>
2026-08-17 01:15:44 +0300

    feat: add exam domain

---
+++ src/main/java/hei/school/app/model/Exam.java
@@ -0,0 +1,12 @@
+package hei.school.app.model;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.UUID;
+
+import lombok.Builder;
+
+@Builder
+public record Exam(UUID id, Instant examDate, BigDecimal coefficient, Course course) {
+
+}

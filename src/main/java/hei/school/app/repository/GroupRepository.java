0c4df6430d4cfebd86e46601e6547cd83a6a52d0
Lavorary <hei.lavorary@gmail.com>
2026-08-17 19:55:43 +0300

    feat : implement repositories for models (#4)
    * feat : add repository for Course
    
    * feat : add repository for Cursus
    
    * feat : add repository for Exam
    
    * feat : add repository for Grade
    
    * feat : add repository for GroupMembership
    
    * feat : add repository for Group
    
    * feat : add repository for ScoreHistory
    
    * feat : add repository for User
    
    * feat : add repositories for the models

---
+++ src/main/java/hei/school/app/repository/GroupRepository.java
@@ -0,0 +1,13 @@
+package hei.school.app.repository;
+
+import hei.school.app.repository.model.JGroup;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupRepository extends JpaRepository<JGroup, UUID> {
+  Optional<JGroup> findByRef(String ref);
+
+  List<JGroup> findBuCursus_id(UUID cursusId);
+}

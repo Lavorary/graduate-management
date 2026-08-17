301922601027e8dc153214e95b77e6f9041ad624
Lavorary <hei.lavorary@gmail.com>
2026-08-17 12:21:20 +0300

    feat : add repository for Course

---
+++ src/main/java/hei/school/app/repository/CourseRepository.java
@@ -0,0 +1,14 @@
+package hei.school.app.repository;
+
+import hei.school.app.repository.model.JCourse;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CourseRepository extends JpaRepository<JCourse, UUID> {
+    Optional<JCourse> findByRef(String ref);
+    List<JCourse> findByCursusId(UUID cursusId);
+    List<JCourse> findByTeachers_Id(UUID teacherId);
+}

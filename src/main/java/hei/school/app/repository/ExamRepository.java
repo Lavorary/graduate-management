03835c1ed295c8839e30024ba5fc906a982b5793
Diarilala <hei.diarilala@gmail.com>
2026-08-19 22:58:01 +0300

    feat: add findCourseIdByExamId, findCursusIdByExamId

--- src/main/java/hei/school/app/repository/ExamRepository.java
+++ src/main/java/hei/school/app/repository/ExamRepository.java
@@ -2,9 +2,17 @@ package hei.school.app.repository;
 
 import hei.school.app.repository.model.JExam;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ExamRepository extends JpaRepository<JExam, UUID> {
   List<JExam> findByCourseId(UUID courseId);
+
+  @Query("select e.course.id from JExam e where e.id = :examId")
+  Optional<UUID> findCourseIdByExamId(UUID examId);
+
+  @Query("select e.course.cursus.id from JExam e where e.id = :examId")
+  Optional<UUID> findCursusIdByExamId(UUID examId);
 }

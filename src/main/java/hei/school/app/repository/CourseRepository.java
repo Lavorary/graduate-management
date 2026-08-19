09f1fc2cde2384ca9eb5efefbc46c10cb13c161b
Lavorary <hei.lavorary@gmail.com>
2026-08-19 14:05:23 +0300

    fix : remove useless space between codes

--- src/main/java/hei/school/app/repository/CourseRepository.java
+++ src/main/java/hei/school/app/repository/CourseRepository.java
@@ -8,6 +8,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseRepository extends JpaRepository<JCourse, UUID> {
   Optional<JCourse> findByRef(String ref);
+
   List<JCourse> findByCursusId(UUID cursusId);
+
   List<JCourse> findByTeachers_Id(UUID teacherId);
 }

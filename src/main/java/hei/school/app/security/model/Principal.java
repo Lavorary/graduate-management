31c002611fcfcf1da953970ad0ae00b4df380952
Diarilala <hei.diarilala@gmail.com>
2026-08-19 22:27:17 +0300

    feat: add principal for handling user details

--- src/main/java/hei/school/app/security/model/Principal.java
+++ src/main/java/hei/school/app/security/model/Principal.java
@@ -1,34 +1,46 @@
 package hei.school.app.security.model;
 
+import hei.school.app.model.User;
 import java.util.Collection;
 import java.util.List;
-
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-import hei.school.app.model.User;
-
 public record Principal(User user) implements UserDetails {
 
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority("ROLE_" + user.role().name()));
-    }
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(new SimpleGrantedAuthority("ROLE_" + user.role().name()));
+  }
 
-    @Override
-    public String getUsername() { return user.email(); }
+  @Override
+  public String getUsername() {
+    return user.email();
+  }
 
-    @Override
-    public String getPassword() { return user.password(); }
+  @Override
+  public String getPassword() {
+    return user.password();
+  }
 
-    @Override
-    public boolean isAccountNonExpired() { return true; }
-    @Override
-    public boolean isAccountNonLocked() { return true; }
-    @Override
-    public boolean isCredentialsNonExpired() { return true; }
-    @Override
-    public boolean isEnabled() { return true; }
+  @Override
+  public boolean isAccountNonExpired() {
+    return true;
+  }
 
+  @Override
+  public boolean isAccountNonLocked() {
+    return true;
+  }
+
+  @Override
+  public boolean isCredentialsNonExpired() {
+    return true;
+  }
+
+  @Override
+  public boolean isEnabled() {
+    return true;
+  }
 }

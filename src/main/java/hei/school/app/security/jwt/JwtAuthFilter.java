97e49e9cbf0a10cea83af3acc58e504cdc86e710
Diarilala <hei.diarilala@gmail.com>
2026-08-19 23:49:18 +0300

    fix: make jwtAuthFilter a bean

--- src/main/java/hei/school/app/security/jwt/JwtAuthFilter.java
+++ src/main/java/hei/school/app/security/jwt/JwtAuthFilter.java
@@ -10,8 +10,10 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+@Component
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter {
   private final JwtService jwtService;

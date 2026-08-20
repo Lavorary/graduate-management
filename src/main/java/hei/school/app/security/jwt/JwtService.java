3bf55295db17cfd3149ec252f3c24fb040f3093a
Lavorary <hei.lavorary@gmail.com>
2026-08-20 15:35:26 +0300

    fix : add missing @Service in JwtService

--- src/main/java/hei/school/app/security/jwt/JwtService.java
+++ src/main/java/hei/school/app/security/jwt/JwtService.java
@@ -11,7 +11,9 @@ import java.nio.charset.StandardCharsets;
 import java.util.Date;
 import javax.crypto.SecretKey;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
+@Service
 public class JwtService {
   private final SecretKey key;
   private final long expirationMs;

d4ac12f870db99e6a2c81365bba907427f9802f3
Lavorary <hei.lavorary@gmail.com>
2026-08-20 09:45:10 +0300

    feat: add check for course by id and teacher, cursus by course id, add function for all course by teacher id, add find all course by cursus id

--- src/main/java/hei/school/app/repository/CourseRepository.java
+++ src/main/java/hei/school/app/repository/CourseRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface CourseRepository extends JpaRepository<JCourse, UUID> {
   Optional<JCourse> findByRef(String ref);
@@ -12,4 +13,23 @@ public interface CourseRepository extends JpaRepository<JCourse, UUID> {
   List<JCourse> findByCursusId(UUID cursusId);
 
   List<JCourse> findByTeachers_Id(UUID teacherId);
+
+  @Query(
+      """
+      select case when count(c) > 0 then true else false end
+      from JCourse c join c.teachers t
+      where c.id = :courseId and t.id = :teacherId
+      """)
+  boolean existsByIdAndTeacherId(UUID courseId, UUID teacherId);
+
+  @Query(
+      """
+      select c.cursus.id from JCourse c where c.id = :courseId
+      """)
+  Optional<UUID> findCursusIdByCourseId(UUID courseId);
+
+  @Query("select c from JCourse c join c.teachers t where t.id = :teacherId")
+  List<JCourse> findAllTaughtByTeacherId(UUID teacherId);
+
+  List<JCourse> findAllByCursusIdIn(List<UUID> cursusIds);
 }
